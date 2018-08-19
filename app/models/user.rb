973214cb8b8579bfbbfa5984ea76c911cc6476d6
Bruno Padilha <bpadilha.mech@gmail.com>
2018-08-19 13:12:34 -0300

    Atualização no relacionamento de usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,31 @@ class User < ApplicationRecord
 
   has_many :tweets
 
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreing_key: "follower_id",
+                                  dependent: :destroy
+
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreing_key: 'followed_id'
+                                   dependent: :destroy
+
+
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :followers, through: :passive_relationships, source: :follower
+
+
+  def following? (other_user)
+    #este metodo chega se um usuário está seguindo o outro.
+  end
+
+  def follow!(other_user)
+    #este método criará o relacionamento entre um usuário e outro.
+  end
+
+  def unfollow! (other_user)
+    #este método apagará o relacionamento entre um usuario e outro
+  end
+
+  
 end
